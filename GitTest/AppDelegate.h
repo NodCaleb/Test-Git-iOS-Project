dc4ebbd5501eab824b419b24a9d9e4a986f489ad
Eugene Rozhkov <eugenerozhkov@Teiwaz.lan>
2014-10-24 13:28:46 +0300

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Eugene Rozhkov on 24.10.14.
+//  Copyright (c) 2014 nordPoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Eugene Rozhkov on 24.10.14.
+//  Copyright (c) 2014 nordPoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
