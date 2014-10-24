aefc264a1ad05aa3a0dde8d61a2cf3577dae0120
Eugene Rozhkov <eugenerozhkov@Teiwaz.lan>
2014-10-24 13:47:18 +0300

    added TestClass

---
+++ TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  GitTest
+//
+//  Created by Eugene Rozhkov on 24.10.14.
+//  Copyright (c) 2014 nordPoint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  GitTest
+//
+//  Created by Eugene Rozhkov on 24.10.14.
+//  Copyright (c) 2014 nordPoint. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
